471a76238e80c8600bff66d3ae495605b5758acc
Oleh <eddrfgthyjn@gmail.com>
2025-03-30 21:11:26 +0300

    .

--- src/components/ActualToursSection/ActualToursSection.tsx
+++ src/components/ActualToursSection/ActualToursSection.tsx
@@ -1,213 +1,213 @@
-// import { useEffect, useState } from "react";
-// import { Layout } from "../Layout/Layout";
-// import s from "./ActualToursSection.module.css";
-// import { PopupTour } from "../PopupTour/PopupTour";
+import { useEffect, useState } from "react";
+import { Layout } from "../Layout/Layout";
+import s from "./ActualToursSection.module.css";
+import { PopupTour } from "../PopupTour/PopupTour";
 
-// import Lenis from "lenis";
-// import { useQuery } from "@tanstack/react-query";
-// import { API_URL } from "../../App";
-// import axios from "axios";
-// const lenis = new Lenis();
-// function raf(time: number) {
-//   lenis.raf(time);
-//   requestAnimationFrame(raf);
-// }
-// requestAnimationFrame(raf);
+import Lenis from "lenis";
+import { useQuery } from "@tanstack/react-query";
+import { API_URL } from "../../App";
+import axios from "axios";
+const lenis = new Lenis();
+function raf(time: number) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
 
-// export interface Day {
-//   hl_input_title: string;
-//   hl_image_image: string;
-//   hl_input_way: string;
-//   hl_input_distance: string;
-//   hl_input_description: string;
-// }
+export interface Day {
+  hl_input_title: string;
+  hl_image_image: string;
+  hl_input_way: string;
+  hl_input_distance: string;
+  hl_input_description: string;
+}
 
-// interface TourType {
-//   id: number;
-//   title: { rendered: string };
-//   load_image_text_main_image: string;
-//   input_location?: string;
-//   price?: string;
-//   input_date_start: string;
-//   input_date_end: string;
-//   input_main_price: string;
-//   input_route: string;
-//   save_data_text: Day[];
-// }
+interface TourType {
+  id: number;
+  title: { rendered: string };
+  load_image_text_main_image: string;
+  input_location?: string;
+  price?: string;
+  input_date_start: string;
+  input_date_end: string;
+  input_main_price: string;
+  input_route: string;
+  save_data_text: Day[];
+}
 
-// const fetchGallery = async () => {
-//   const { data } = await axios.get(`${API_URL}wp-json/wp/v2/tour`);
-//   return data;
-// };
+const fetchGallery = async () => {
+  const { data } = await axios.get(`${API_URL}wp-json/wp/v2/tour`);
+  return data;
+};
 
-// export const yearEditor = (start: string, end: string) => {
-//   const startDate = new Date(start);
-//   const endDate = new Date(end);
+export const yearEditor = (start: string, end: string) => {
+  const startDate = new Date(start);
+  const endDate = new Date(end);
 
-//   const formattedStartDate = startDate.toLocaleDateString("uk-UA", {
-//     day: "2-digit",
-//     month: "2-digit",
-//   });
+  const formattedStartDate = startDate.toLocaleDateString("uk-UA", {
+    day: "2-digit",
+    month: "2-digit",
+  });
 
-//   const formattedEndDate = endDate.toLocaleDateString("uk-UA", {
-//     day: "2-digit",
-//     month: "2-digit",
-//   });
+  const formattedEndDate = endDate.toLocaleDateString("uk-UA", {
+    day: "2-digit",
+    month: "2-digit",
+  });
 
-//   return `${formattedStartDate} – ${formattedEndDate}`;
-// };
+  return `${formattedStartDate} – ${formattedEndDate}`;
+};
 
-// const getYear = (date: string) => {
-//   const dateObj = new Date(date);
-//   return dateObj.getFullYear(); // Отримаємо рік
-// };
+const getYear = (date: string) => {
+  const dateObj = new Date(date);
+  return dateObj.getFullYear(); // Отримаємо рік
+};
 
-// export const ActualToursSection = () => {
-//   const [activeTourId, setActiveTourId] = useState<number | null>(null);
+export const ActualToursSection = () => {
+  const [activeTourId, setActiveTourId] = useState<number | null>(null);
 
-//   const { data = [], isLoading } = useQuery({
-//     queryKey: ["tours"],
-//     queryFn: fetchGallery,
-//   });
+  const { data = [], isLoading } = useQuery({
+    queryKey: ["tours"],
+    queryFn: fetchGallery,
+  });
 
-//   const dateEditor = (start: string, end: string) => {
-//     const startDate = new Date(start);
-//     const endDate = new Date(end);
+  const dateEditor = (start: string, end: string) => {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
 
-//     const startDay = startDate.getDate();
+    const startDay = startDate.getDate();
 
-//     const formattedEndDate = endDate.toLocaleDateString("uk-UA", {
-//       day: "2-digit",
-//       month: "2-digit",
-//     });
+    const formattedEndDate = endDate.toLocaleDateString("uk-UA", {
+      day: "2-digit",
+      month: "2-digit",
+    });
 
-//     const formattedStartDay = startDay < 10 ? "0" + startDay : startDay;
+    const formattedStartDay = startDay < 10 ? "0" + startDay : startDay;
 
-//     return `${formattedStartDay} – ${formattedEndDate}`;
-//   };
+    return `${formattedStartDay} – ${formattedEndDate}`;
+  };
 
-//   useEffect(() => {
-//     if (activeTourId) {
-//       document.body.style.overflow = "hidden";
-//     } else {
-//       document.body.style.overflow = "";
-//     }
+  useEffect(() => {
+    if (activeTourId) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
 
-//     return () => {
-//       document.body.style.overflow = "";
-//     };
-//   }, [activeTourId]);
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [activeTourId]);
 
-//   const handleTourPopup = (tourId: number) => {
-//     setActiveTourId(activeTourId === tourId ? null : tourId);
-//   };
+  const handleTourPopup = (tourId: number) => {
+    setActiveTourId(activeTourId === tourId ? null : tourId);
+  };
 
-//   if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <p>Loading...</p>;
 
-//   return (
-//     <section className={s.section}>
-//       <Layout>
-//         <h2>
-//           Актуальные
-//           <br /> event-туры
-//         </h2>
+  return (
+    <section className={s.section}>
+      <Layout>
+        <h2>
+          Актуальные
+          <br /> event-туры
+        </h2>
 
-//         <div className={s.list}>
-//           {data.map((item: TourType) => (
-//             <div className={s.item} key={item.id}>
-//               <div
-//                 style={{
-//                   backgroundImage: `url(${item.load_image_text_main_image})`,
-//                   backgroundSize: "cover",
-//                   backgroundPosition: "center",
-//                   width: "100%",
-//                 }}
-//                 className={s.imageContainer}
-//               >
-//                 <h3>{item.title.rendered}</h3>
+        <div className={s.list}>
+          {data.map((item: TourType) => (
+            <div className={s.item} key={item.id}>
+              <div
+                style={{
+                  backgroundImage: `url(${item.load_image_text_main_image})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  width: "100%",
+                }}
+                className={s.imageContainer}
+              >
+                <h3>{item.title.rendered}</h3>
 
-//                 <div>
-//                   <span>
-//                     {dateEditor(
-//                       item.input_date_start || "",
-//                       item.input_date_end || ""
-//                     )}
-//                   </span>
-//                 </div>
+                <div>
+                  <span>
+                    {dateEditor(
+                      item.input_date_start || "",
+                      item.input_date_end || ""
+                    )}
+                  </span>
+                </div>
 
-//                 <div className={s.verticalText}>{item.input_route}</div>
-//               </div>
+                <div className={s.verticalText}>{item.input_route}</div>
+              </div>
 
-//               <div>
-//                 <div className={s.bottomHeading}>
-//                   <div>
-//                     <h4>{item.title.rendered}</h4>
-//                     <div className={s.location}>
-//                       <span>{item.input_location}</span>
-//                       <svg
-//                         viewBox="0 0 20 20"
-//                         fill="none"
-//                         xmlns="http://www.w3.org/2000/svg"
-//                       >
-//                         <g opacity="0.6" clipPath="url(#clip0_1340_1948)">
-//                           <path
-//                             d="M6.66667 10H5C4.08083 10 3.33333 10.7475 3.33333 11.6667V13.3333C3.33333 14.2525 4.08083 15 5 15H6.66667C7.58583 15 8.33333 14.2525 8.33333 13.3333V11.6667C8.33333 10.7475 7.58583 10 6.66667 10ZM5 13.3333V11.6667H6.66667V13.3333H5ZM15.8333 1.66667H15V0.833333C15 0.373333 14.6275 0 14.1667 0C13.7058 0 13.3333 0.373333 13.3333 0.833333V1.66667H6.66667V0.833333C6.66667 0.373333 6.29417 0 5.83333 0C5.3725 0 5 0.373333 5 0.833333V1.66667H4.16667C1.86917 1.66667 0 3.53583 0 5.83333V15.8333C0 18.1308 1.86917 20 4.16667 20H15.8333C18.1308 20 20 18.1308 20 15.8333V5.83333C20 3.53583 18.1308 1.66667 15.8333 1.66667ZM4.16667 3.33333H15.8333C17.2117 3.33333 18.3333 4.455 18.3333 5.83333V6.66667H1.66667V5.83333C1.66667 4.455 2.78833 3.33333 4.16667 3.33333ZM15.8333 18.3333H4.16667C2.78833 18.3333 1.66667 17.2117 1.66667 15.8333V8.33333H18.3333V15.8333C18.3333 17.2117 17.2117 18.3333 15.8333 18.3333Z"
-//                             fill="white"
-//                           />
-//                         </g>
-//                         <defs>
-//                           <clipPath id="clip0_1340_1948">
-//                             <rect width="20" height="20" fill="white" />
-//                           </clipPath>
-//                         </defs>
-//                       </svg>
+              <div>
+                <div className={s.bottomHeading}>
+                  <div>
+                    <h4>{item.title.rendered}</h4>
+                    <div className={s.location}>
+                      <span>{item.input_location}</span>
+                      <svg
+                        viewBox="0 0 20 20"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <g opacity="0.6" clipPath="url(#clip0_1340_1948)">
+                          <path
+                            d="M6.66667 10H5C4.08083 10 3.33333 10.7475 3.33333 11.6667V13.3333C3.33333 14.2525 4.08083 15 5 15H6.66667C7.58583 15 8.33333 14.2525 8.33333 13.3333V11.6667C8.33333 10.7475 7.58583 10 6.66667 10ZM5 13.3333V11.6667H6.66667V13.3333H5ZM15.8333 1.66667H15V0.833333C15 0.373333 14.6275 0 14.1667 0C13.7058 0 13.3333 0.373333 13.3333 0.833333V1.66667H6.66667V0.833333C6.66667 0.373333 6.29417 0 5.83333 0C5.3725 0 5 0.373333 5 0.833333V1.66667H4.16667C1.86917 1.66667 0 3.53583 0 5.83333V15.8333C0 18.1308 1.86917 20 4.16667 20H15.8333C18.1308 20 20 18.1308 20 15.8333V5.83333C20 3.53583 18.1308 1.66667 15.8333 1.66667ZM4.16667 3.33333H15.8333C17.2117 3.33333 18.3333 4.455 18.3333 5.83333V6.66667H1.66667V5.83333C1.66667 4.455 2.78833 3.33333 4.16667 3.33333ZM15.8333 18.3333H4.16667C2.78833 18.3333 1.66667 17.2117 1.66667 15.8333V8.33333H18.3333V15.8333C18.3333 17.2117 17.2117 18.3333 15.8333 18.3333Z"
+                            fill="white"
+                          />
+                        </g>
+                        <defs>
+                          <clipPath id="clip0_1340_1948">
+                            <rect width="20" height="20" fill="white" />
+                          </clipPath>
+                        </defs>
+                      </svg>
 
-//                       <span>
-//                         {getYear(item.input_date_start) || ""}.
-//                         {yearEditor(
-//                           item.input_date_start,
-//                           item.input_date_end
-//                         ) || ""}
-//                       </span>
-//                     </div>
-//                   </div>
+                      <span>
+                        {getYear(item.input_date_start) || ""}.
+                        {yearEditor(
+                          item.input_date_start,
+                          item.input_date_end
+                        ) || ""}
+                      </span>
+                    </div>
+                  </div>
 
-//                   <div className={s.priceBlock}>
-//                     <svg
-//                       viewBox="0 0 42 20"
-//                       fill="none"
-//                       xmlns="http://www.w3.org/2000/svg"
-//                     >
-//                       <path
-//                         d="M10 10C12.7614 10 15 7.76142 15 5C15 2.23858 12.7614 0 10 0C7.23858 0 5 2.23858 5 5C5 7.76142 7.23858 10 10 10Z"
-//                         fill="white"
-//                       />
-//                       <path
-//                         d="M10 11.6665C5.85977 11.6711 2.50461 15.0263 2.5 19.1665C2.5 19.6267 2.87309 19.9998 3.33332 19.9998H16.6666C17.1269 19.9998 17.5 19.6267 17.5 19.1665C17.4954 15.0263 14.1402 11.6711 10 11.6665Z"
-//                         fill="white"
-//                       />
-//                       <path
-//                         d="M31.672 17V8.288H28.396V6.272H28.936C30.646 6.272 31.69 5.372 31.816 4.022H34.318V17H31.672Z"
-//                         fill="white"
-//                       />
-//                     </svg>
-//                     / <span>от</span> {item.input_main_price || "Не указано"}
-//                   </div>
-//                 </div>
-//               </div>
+                  <div className={s.priceBlock}>
+                    <svg
+                      viewBox="0 0 42 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M10 10C12.7614 10 15 7.76142 15 5C15 2.23858 12.7614 0 10 0C7.23858 0 5 2.23858 5 5C5 7.76142 7.23858 10 10 10Z"
+                        fill="white"
+                      />
+                      <path
+                        d="M10 11.6665C5.85977 11.6711 2.50461 15.0263 2.5 19.1665C2.5 19.6267 2.87309 19.9998 3.33332 19.9998H16.6666C17.1269 19.9998 17.5 19.6267 17.5 19.1665C17.4954 15.0263 14.1402 11.6711 10 11.6665Z"
+                        fill="white"
+                      />
+                      <path
+                        d="M31.672 17V8.288H28.396V6.272H28.936C30.646 6.272 31.69 5.372 31.816 4.022H34.318V17H31.672Z"
+                        fill="white"
+                      />
+                    </svg>
+                    / <span>от</span> {item.input_main_price || "Не указано"}
+                  </div>
+                </div>
+              </div>
 
-//               <div className={s.btnContainer}>
-//                 <div>Забронировать</div>
-//                 <div onClick={() => handleTourPopup(item.id)}>Детальнее</div>
-//               </div>
+              <div className={s.btnContainer}>
+                <div>Забронировать</div>
+                <div onClick={() => handleTourPopup(item.id)}>Детальнее</div>
+              </div>
 
-//               {activeTourId === item.id && (
-//                 <PopupTour info={item} onClose={() => setActiveTourId(null)} />
-//               )}
-//             </div>
-//           ))}
-//         </div>
-//       </Layout>
-//     </section>
-//   );
-// };
+              {activeTourId === item.id && (
+                <PopupTour info={item} onClose={() => setActiveTourId(null)} />
+              )}
+            </div>
+          ))}
+        </div>
+      </Layout>
+    </section>
+  );
+};
--- src/components/ChroniclesSection/ChroniclesSection.tsx
+++ src/components/ChroniclesSection/ChroniclesSection.tsx
@@ -1,165 +1,165 @@
-// import { useEffect, useRef, useState } from "react";
-// import { Layout } from "../Layout/Layout";
-// import { SiteButton } from "../SiteButton/SiteButton";
-// import { Swiper, SwiperSlide } from "swiper/react";
-// import { Navigation } from "swiper/modules";
-// import "swiper/css";
-// import "swiper/css/navigation";
-// import s from "./ChroniclesSection.module.css";
-// import axios from "axios";
-// import { useQuery } from "@tanstack/react-query";
-// import { API_URL } from "../../App";
+import { useEffect, useRef, useState } from "react";
+import { Layout } from "../Layout/Layout";
+import { SiteButton } from "../SiteButton/SiteButton";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import s from "./ChroniclesSection.module.css";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { API_URL } from "../../App";
 
-// interface ImageItem {
-//   cars: string[];
-// }
+interface ImageItem {
+  cars: string[];
+}
 
-// const fetchGallery = async () => {
-//   const { data } = await axios.get(`${API_URL}wp-json/wp/v2/travel_journal`);
-//   return data;
-// };
+const fetchGallery = async () => {
+  const { data } = await axios.get(`${API_URL}wp-json/wp/v2/travel_journal`);
+  return data;
+};
 
-// export const ChroniclesSection = () => {
-//   const { data = [], isLoading } = useQuery({
-//     queryKey: ["gallery"],
-//     queryFn: fetchGallery,
-//   });
+export const ChroniclesSection = () => {
+  const { data = [], isLoading } = useQuery({
+    queryKey: ["gallery"],
+    queryFn: fetchGallery,
+  });
 
-//   const [selectedCar, setSelectedCar] = useState<string | null>(null);
-//   const prevRef = useRef<HTMLDivElement>(null);
-//   const nextRef = useRef<HTMLDivElement>(null);
-//   const [progress, setProgress] = useState((1 / data.length) * 100);
+  const [selectedCar, setSelectedCar] = useState<string | null>(null);
+  const prevRef = useRef<HTMLDivElement>(null);
+  const nextRef = useRef<HTMLDivElement>(null);
+  const [progress, setProgress] = useState((1 / data.length) * 100);
 
-//   const uniqueCars: string[] = Array.from(
-//     new Set(data.flatMap((item: ImageItem) => item.cars))
-//   );
+  const uniqueCars: string[] = Array.from(
+    new Set(data.flatMap((item: ImageItem) => item.cars))
+  );
 
-//   const filteredData = selectedCar
-//     ? data.filter((item: ImageItem) => item.cars.includes(selectedCar))
-//     : data;
+  const filteredData = selectedCar
+    ? data.filter((item: ImageItem) => item.cars.includes(selectedCar))
+    : data;
 
-//   useEffect(() => {
-//     setProgress((1 / filteredData.length) * 100);
-//   }, [selectedCar, filteredData]);
+  useEffect(() => {
+    setProgress((1 / filteredData.length) * 100);
+  }, [selectedCar, filteredData]);
 
-//   if (isLoading) return <p>Loading...</p>;
+  if (isLoading) return <p>Loading...</p>;
 
-//   return (
-//     <section className={s.section}>
-//       <Layout>
-//         <div className={s.titleContainer}>
-//           <h2>Хроники путешествий</h2>
-//           <SiteButton />
-//         </div>
+  return (
+    <section className={s.section}>
+      <Layout>
+        <div className={s.titleContainer}>
+          <h2>Хроники путешествий</h2>
+          <SiteButton />
+        </div>
 
-//         {/* Прогрес-бар */}
-//         <div className={s.progressBarContainer}>
-//           <div
-//             className={s.progressBar}
-//             style={{ width: `${progress}%` }}
-//           ></div>
-//         </div>
+        {/* Прогрес-бар */}
+        <div className={s.progressBarContainer}>
+          <div
+            className={s.progressBar}
+            style={{ width: `${progress}%` }}
+          ></div>
+        </div>
 
-//         <div className={s.tabController}>
-//           {uniqueCars.map((car, index) => (
-//             <div
-//               key={index}
-//               onClick={() => setSelectedCar(car === selectedCar ? null : car)}
-//               className={selectedCar === car ? s.active : ""}
-//             >
-//               {car}
-//               <span>
-//                 (
-//                 {
-//                   data.filter((item: ImageItem) => item.cars.includes(car))
-//                     .length
-//                 }
-//                 )
-//               </span>
-//             </div>
-//           ))}
-//         </div>
-//       </Layout>
+        <div className={s.tabController}>
+          {uniqueCars.map((car, index) => (
+            <div
+              key={index}
+              onClick={() => setSelectedCar(car === selectedCar ? null : car)}
+              className={selectedCar === car ? s.active : ""}
+            >
+              {car}
+              <span>
+                (
+                {
+                  data.filter((item: ImageItem) => item.cars.includes(car))
+                    .length
+                }
+                )
+              </span>
+            </div>
+          ))}
+        </div>
+      </Layout>
 
-//       <div className={s.sliderWrapper}>
-//         {/* Кнопки навігації */}
-//         <div className={s.swiperController}>
-//           <div ref={prevRef} className={s.btn}>
-//             <svg
-//               viewBox="0 0 32 32"
-//               fill="none"
-//               xmlns="http://www.w3.org/2000/svg"
-//             >
-//               <path
-//                 d="M23.0222 31.8507L23.9648 30.9081L9.52884 16.4721C9.40218 16.3454 9.33418 16.1787 9.33418 16.0001C9.33418 15.8214 9.40351 15.6547 9.52884 15.5281L23.9648 1.09208L23.0222 0.149414L8.58618 14.5854C8.20885 14.9627 8.00084 15.4654 8.00084 16.0001C8.00084 16.5347 8.20885 17.0361 8.58618 17.4147L23.0222 31.8507Z"
-//                 fill="white"
-//               />
-//             </svg>
-//           </div>
+      <div className={s.sliderWrapper}>
+        {/* Кнопки навігації */}
+        <div className={s.swiperController}>
+          <div ref={prevRef} className={s.btn}>
+            <svg
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M23.0222 31.8507L23.9648 30.9081L9.52884 16.4721C9.40218 16.3454 9.33418 16.1787 9.33418 16.0001C9.33418 15.8214 9.40351 15.6547 9.52884 15.5281L23.9648 1.09208L23.0222 0.149414L8.58618 14.5854C8.20885 14.9627 8.00084 15.4654 8.00084 16.0001C8.00084 16.5347 8.20885 17.0361 8.58618 17.4147L23.0222 31.8507Z"
+                fill="white"
+              />
+            </svg>
+          </div>
 
-//           <div ref={nextRef} className={s.btn}>
-//             <svg
-//               viewBox="0 0 32 32"
-//               fill="none"
-//               xmlns="http://www.w3.org/2000/svg"
-//             >
-//               <path
-//                 d="M8.97782 31.8507L8.03516 30.9081L22.4712 16.4721C22.5978 16.3454 22.6658 16.1787 22.6658 16.0001C22.6658 15.8214 22.5965 15.6547 22.4712 15.5281L8.03516 1.09208L8.97782 0.149414L23.4138 14.5854C23.7912 14.9627 23.9992 15.4654 23.9992 16.0001C23.9992 16.5347 23.7912 17.0361 23.4138 17.4147L8.97782 31.8507Z"
-//                 fill="white"
-//               />
-//             </svg>
-//           </div>
-//         </div>
+          <div ref={nextRef} className={s.btn}>
+            <svg
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M8.97782 31.8507L8.03516 30.9081L22.4712 16.4721C22.5978 16.3454 22.6658 16.1787 22.6658 16.0001C22.6658 15.8214 22.5965 15.6547 22.4712 15.5281L8.03516 1.09208L8.97782 0.149414L23.4138 14.5854C23.7912 14.9627 23.9992 15.4654 23.9992 16.0001C23.9992 16.5347 23.7912 17.0361 23.4138 17.4147L8.97782 31.8507Z"
+                fill="white"
+              />
+            </svg>
+          </div>
+        </div>
 
-//         <Swiper
-//           spaceBetween={20}
-//           slidesPerView={1}
-//           initialSlide={0}
-//           navigation={{ prevEl: prevRef.current, nextEl: nextRef.current }}
-//           modules={[Navigation]}
-//           onInit={(swiper) => {
-//             if (
-//               swiper.params.navigation &&
-//               typeof swiper.params.navigation !== "boolean"
-//             ) {
-//               swiper.params.navigation.prevEl = prevRef.current;
-//               swiper.params.navigation.nextEl = nextRef.current;
-//               swiper.navigation.init();
-//               swiper.navigation.update();
-//             }
-//           }}
-//           onSlideChange={(swiper) => {
-//             const progress =
-//               ((swiper.activeIndex + 1) / swiper.slides.length) * 100;
-//             setProgress(progress);
-//           }}
-//           className={s.swiperContainer}
-//         >
-//           {filteredData.map(
-//             (image: {
-//               id: number;
-//               load_image_text_photo: string;
-//               input_way_start: string;
-//               input_way_end: string;
-//               input_date: string;
-//             }) =>
-//               image.load_image_text_photo ? (
-//                 <SwiperSlide key={image.id} className={s.slide}>
-//                   <img src={image.load_image_text_photo} alt="" />
-//                   <div className={s.slideBottomInfo}>
-//                     <p className={s.route}>
-//                       {image.input_way_start}
-//                       <span></span>
-//                       {image.input_way_end}
-//                     </p>
+        <Swiper
+          spaceBetween={20}
+          slidesPerView={1}
+          initialSlide={0}
+          navigation={{ prevEl: prevRef.current, nextEl: nextRef.current }}
+          modules={[Navigation]}
+          onInit={(swiper) => {
+            if (
+              swiper.params.navigation &&
+              typeof swiper.params.navigation !== "boolean"
+            ) {
+              swiper.params.navigation.prevEl = prevRef.current;
+              swiper.params.navigation.nextEl = nextRef.current;
+              swiper.navigation.init();
+              swiper.navigation.update();
+            }
+          }}
+          onSlideChange={(swiper) => {
+            const progress =
+              ((swiper.activeIndex + 1) / swiper.slides.length) * 100;
+            setProgress(progress);
+          }}
+          className={s.swiperContainer}
+        >
+          {filteredData.map(
+            (image: {
+              id: number;
+              load_image_text_photo: string;
+              input_way_start: string;
+              input_way_end: string;
+              input_date: string;
+            }) =>
+              image.load_image_text_photo ? (
+                <SwiperSlide key={image.id} className={s.slide}>
+                  <img src={image.load_image_text_photo} alt="" />
+                  <div className={s.slideBottomInfo}>
+                    <p className={s.route}>
+                      {image.input_way_start}
+                      <span></span>
+                      {image.input_way_end}
+                    </p>
 
-//                     <p>{image.input_date}</p>
-//                   </div>
-//                 </SwiperSlide>
-//               ) : null
-//           )}
-//         </Swiper>
-//       </div>
-//     </section>
-//   );
-// };
+                    <p>{image.input_date}</p>
+                  </div>
+                </SwiperSlide>
+              ) : null
+          )}
+        </Swiper>
+      </div>
+    </section>
+  );
+};
